f05c38e8c614071a1ab0f9f6d35c4a4fbc25768b
yongin <kkc926@naver.com>
2020-12-16 01:57:04 +0900

    1/6

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,26 +1,42 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-header 
+    class="bg-white text-grey-10"
+    bordered
+    >
+      <q-toolbar class="constrain">
         <q-btn
           flat
+          rount
           dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+          size="18px"
+          icon="eva-camera-outline" 
+          to="/camera"
+          class="large-screen-only q-mr-sm"
+          />
+          <q-separator vertical
+          spaced
+          class="large-screen-only"/>
+          
+        <q-toolbar-title
+        class="text-grand-hotel text-bold">
+          BetweenCloset
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+      <q-btn
+          flat
+          rount
+          dense
+          size="18px"
+          icon="eva-home-outline" 
+          to="/"
+          class="large-screen-only"
+          />
       </q-toolbar>
+
     </q-header>
 
       <q-footer
-        class="bg-white"
+        class="bg-white small-screen-only"
         bordered>
 
         <q-tabs
@@ -54,3 +70,20 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 77px
+  .q-toolbar__title
+    font-size: 30px
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
+    
+
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+
+
+</style>
