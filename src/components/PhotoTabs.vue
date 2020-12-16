da8a0b83ea1a1032411333c1867a7cab487a1db4
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-12-16 20:16:59 +0900

    profile page layout

---
+++ src/components/PhotoTabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="q-pa">
+    <div class="q-gutter-y" style="max-width: 800px">
+      <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="mails" label="Mails" />
+          <q-tab name="alarms" label="Alarms" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PhotoTabs',
+  data () {
+    return {
+      tab: 'mails'
+    }
+  }
+}
+</script>
--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,31 +1,42 @@
 <template>
-    <q-item>
-        <q-item-section avatar>
-            <q-avatar size="80px" class="profile">
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <q-item-label caption class="name">사람 이름</q-item-label>
-        </q-item-section>
+    <q-list>
+        <q-item>
+            <q-item-section avatar>
+                <q-avatar size="70px" class="profile">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+                <q-item-label size="14" caption class="name">김민지</q-item-label>
+            </q-item-section>
 
-        <q-item-section>
-            <q-item-label>36</q-item-label>
-            <q-item-label caption>
-            코디
-            </q-item-label>
-        </q-item-section>
-        <q-item-section>
-            <q-item-label>20</q-item-label>
-            <q-item-label caption>
-            친구
-            </q-item-label>
-        </q-item-section>
-    </q-item>
+            <q-item-section>
+                <q-item-label>36</q-item-label>
+                <q-item-label caption>
+                코디
+                </q-item-label>
+            </q-item-section>
+            <q-item-section>
+                <q-item-label>20</q-item-label>
+                <q-item-label caption>
+                친구
+                </q-item-label>
+            </q-item-section>
+        </q-item>
+        <q-item>
+            <photo-tabs/>
+        </q-item>
+    </q-list>
 </template>
 
 <script>
+import PhotoTabs from '../components/PhotoTabs.vue'
+
 export default {
-  name: 'PageProfile'
+  name: 'PageProfile',
+  components: {
+    PhotoTabs
+  },
 }
+
 </script>
 <style scoped>
 .profile{
