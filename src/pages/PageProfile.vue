336da011a4b4238791fda42d2101c8f763605f1e
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-12-18 13:27:57 +0900

    profile 컴포넌트로 이동

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,22 +1,6 @@
 <template>
   <q-page>
-    <q-item>
-      <q-item-section avatar class="col-4">
-        <q-avatar size="70px" class="profile">
-          <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-        </q-avatar>
-        <q-item-label size="14" caption class="name">김민지</q-item-label>
-      </q-item-section>
-
-      <q-item-section class="col-4">
-        <q-item-label class="number">36</q-item-label>
-        <q-item-label caption> 코디 </q-item-label>
-      </q-item-section>
-      <q-item-section class="col-4">
-        <q-item-label class="number">20</q-item-label>
-        <q-item-label caption> 친구 </q-item-label>
-      </q-item-section>
-    </q-item>
+    <user-profile />
     <photo-tabs class="ms" />
   </q-page>
 </template>
@@ -35,18 +19,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-.profile {
-  margin-top: 10px;
-  margin-right: 30px;
-}
-.name {
-  margin-left: 5px;
-  margin-top: 10px;
-  font-size: 16px;
-}
-
-.number {
-  font-size: 20px;
-}
-</style>
